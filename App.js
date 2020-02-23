70be9e4ba90c279fddbf4689ec2765957e3933a9
Irmak Parkis <nihal.pakis@mail.mcgill.ca>
2020-02-23 16:49:33 -0500

    Starting

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import React from 'react';
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Container, Header, Content, Button, Text } from 'native-base';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+
+      <Container>
+        <Header />
+        <Content>
+          <Button light><Text> Light </Text></Button>
+          <Button primary><Text> Primary </Text></Button>
+          <Button success><Text> Success </Text></Button>
+          <Button info><Text> Info </Text></Button>
+          <Button warning><Text> Warning </Text></Button>
+          <Button danger><Text> Danger </Text></Button>
+          <Button dark><Text> Dark </Text></Button>
+        </Content>
+
+      </Container>
+
+    );
+  }
 }
 
 const styles = StyleSheet.create({
